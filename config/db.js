a58ced3fecd42ee36e31c1cc3e51a1ad5d041bda
angeloralaivelo <ralaiveloangelo1427@gmail.com>
2022-01-12 10:12:32 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ let connection = mysql.createConnection({
   host     : 'localhost',
   user     : (process.env.NODE_ENV == 'production')?'ucomadmg_solarpro':'root',
   password : (process.env.NODE_ENV == 'production')?'Solarpro!!':'',
-  database : (process.env.NODE_ENV == 'production')?'ucomadmg_publoc':'publoc_api',
+  database : (process.env.NODE_ENV == 'production')?'ucomadmg_publoc_api':'publoc_api',
   multipleStatements: true
 });
  
