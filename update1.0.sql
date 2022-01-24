09056aa163b0c9258434f2d112f5f30fb9560015
angeloralaivelo <ralaiveloangelo1427@gmail.com>
2022-01-24 16:13:42 +0100

    changement nodejs

--- update1.0.sql
+++ update1.0.sql
@@ -7,5 +7,5 @@ alter table annonceur add ann_is_agence_com int null default 0;
 alter table panneau add pan_list_photo varchar(255) null;
 
 
-alter table panneau add pan_add_by_reg int null default 0;
+alter table panneau add pan_add_by_reg int null;
 alter table panneau add pan_modifiable int null default 1;
