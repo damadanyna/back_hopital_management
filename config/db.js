4d5f606bc0d75c08c8fccf30899da73e5dfd6bdd
unknown <ralaiveloangelo1427@gmail.com>
2022-10-14 11:31:01 +0300

    Gestion ajout patient

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 let mysql      = require('mysql');
 let connection = mysql.createConnection({
   host     : 'localhost',
-  user     : (process.env.NODE_ENV == 'production')?'root':'root',
-  password : (process.env.NODE_ENV == 'production')?'Squadron42!!':'',
-  database : (process.env.NODE_ENV == 'production')?'hopital_base':'hopital_base',
+  user     : (process.env.NODE_ENV == 'production')?'defimg_hopital':'root',
+  password : (process.env.NODE_ENV == 'production')?'Hopital_1234':'',
+  database : (process.env.NODE_ENV == 'production')?'defimg_hopital':'hopital_base',
   multipleStatements: true
 });
  
