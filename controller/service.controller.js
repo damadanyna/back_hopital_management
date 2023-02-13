ff663500b311f99de44ff47a2eec7b91a56e7d18
angeloralaivelo <81219758+angelorlv@users.noreply.github.com>
2023-02-13 11:42:15 +0100

    Gestion correction cat

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -67,7 +67,10 @@ class Service{
             }
 
             _data.service_code = code
-            _data.service_parent_id = null
+            
+            if(parseInt(_data.service_parent_id) == -1){
+                _data.service_parent_id = null
+            }
             
             let _serv = await D.set('service',_data)
 
