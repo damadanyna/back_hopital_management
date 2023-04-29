aa9d02e839af2029e9a4106322fb7bf65bd47dca
unknown <Administrateur@SERVEUR_WK19.HLA.LAN>
2023-04-29 15:54:49 +0300

    correction git

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    setTitle: (title) => ipcRenderer.send('set-title', title),
-    downFact: (pdf) => ipcRenderer.send('load-pdf', pdf),
-    dd: (dd) => ipcRenderer.send('dd', dd),
-})
