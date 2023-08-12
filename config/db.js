82657d16be353cb09561c55fa328b7661b076f53
angelorlv <ralaiveloangelo1427@gmail.com>
2023-08-12 04:54:30 +0200

    correction base de donnée

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 let mysql      = require('mysql');
 let connection = mysql.createConnection({
   host     : 'localhost',
-  user     : (process.env.NODE_ENV == 'production')?'root':'dev',
-  password : (process.env.NODE_ENV == 'production')?'hp_back22!!':'master!!',
+  user     : (process.env.NODE_ENV == 'production')?'root':'root',
+  password : (process.env.NODE_ENV == 'production')?'hp_back22!!':'hp_back22!!',
   // password : (process.env.NODE_ENV == 'production')?'':'',
   database : (process.env.NODE_ENV == 'production')?'hopital_base':'hopital_base',
   multipleStatements: true
